e1c8734080b9204228cf02d068021e2da89a39e1
alexgithub <alexgithub@yandex.ru>
2024-07-11 12:24:40 +1000

    commit-12-2 .github/workflows/web.yml

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -51,12 +51,6 @@ export default class GameController {
     const team = new Team();
     const arrPositionPlayers = [0, 1, 8, 9, 16, 17, 24, 25, 32, 33, 40, 41, 48, 49, 56, 57];
 
-    function randomPositionPlayers() {
-      const randomPosition = arrPositionPlayers[Math.floor(Math.random() * arrPositionPlayers.length)];// Math.floor(Math.random() * 16 - случайное число от 0 до 15
-      arrPositionPlayers.splice(arrPositionPlayers.indexOf(randomPosition), 1); // удаляем номер ячейки который уже использовали
-      return randomPosition;
-    }
-
     const positionedCharacterPlayers = [];
 
     team.charactersPlayers.forEach((item) => {
